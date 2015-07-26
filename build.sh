e0fa354b6b78ba514ad1012ca7e6242b9c2cc714
Techwolfy <dring@g33kworld.net>
2015-07-25 18:32:02 -0700

    Modified build script to allow additional arguments at runtime.

--- build.sh
+++ build.sh
@@ -10,18 +10,18 @@ FILES="*.cpp mod/*.cpp"
 
 #telepresenced
 echo "Building telepresenced..."
-${CC} ${OPTIONS} -o telepresenced ${FILES} ${INCLUDE} ${LIBS}
+${CC} ${OPTIONS} -o telepresenced ${FILES} ${INCLUDE} ${LIBS} $@
 
 #telepresence-client
 echo "Building telepresence-client..."
 #Currently identical to 'telepresenced' binary, will be separated again later
-#${CC} ${OPTIONS} -o telepresence-client ${FILES} ${INCLUDE} ${LIBS}
+#${CC} ${OPTIONS} -o telepresence-client ${FILES} ${INCLUDE} ${LIBS} $@
 cp telepresenced telepresence-client
 
 #telepresence-pololu
 echo "Building telepresence-pololu..."
-${CC} ${OPTIONS} -DPOLOLU -o telepresence-pololu ${FILES} ${INCLUDE} ${LIBS}
+${CC} ${OPTIONS} -DPOLOLU -o telepresence-pololu ${FILES} ${INCLUDE} ${LIBS} $@
 
 #telepresence-raspi
 echo "Building telepresence-raspi..."
-${CC} ${OPTIONS} -DRASPI -o telepresence-raspi ${FILES} ${INCLUDE} ${LIBS}
+${CC} ${OPTIONS} -DRASPI -o telepresence-raspi ${FILES} ${INCLUDE} ${LIBS} $@
